9c75189dacdee5007cc294563318b6805db04bdd
Federico Melis <136093442+fedemelis21@users.noreply.github.com>
2023-11-15 21:34:02 +0100

    upd

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import re
 import string
 import threading
 import time
+from datetime import datetime
+from tokenize import String
 
 import requests as requests
 from github import GithubException, Github
@@ -73,21 +75,36 @@ def retrieve_repo(source, personal_key):
 
 
 def download_readme(username, repo_name, token):
-    url = f'https://api.github.com/repos/{username}/{repo_name}/readme'
+    readme_url = f'https://api.github.com/repos/{username}/{repo_name}/readme'
+    repo_url = f'https://api.github.com/repos/{username}/{repo_name}'
     headers = {'Authorization': f'Bearer {token}'}
 
-    response = requests.get(url, headers=headers)
+    # Fetch README
+    readme_response = requests.get(readme_url, headers=headers)
 
-    if response.status_code == 200:
-        readme_content = response.json()['content']
+    if readme_response.status_code == 200:
+        readme_content = readme_response.json()['content']
         readme_content = base64.b64decode(readme_content).decode('utf-8')
 
-        # save the README.md file
-        with open(os.path.join("data", f"{repo_name}.md"), 'w', encoding='utf-8') as file:
-            file.write(readme_content)
-        print(f"README.md of the {repo_name} repository downloaded and saved.")
+        # Fetch Repository info
+        repo_response = requests.get(repo_url, headers=headers)
+        if repo_response.status_code == 200:
+            last_updated = repo_response.json()['updated_at']
+            last_updated = datetime.strptime(last_updated, '%Y-%m-%dT%H:%M:%SZ')
+            # Convert last_updated to string
+            last_updated = last_updated.strftime('%d-%m-%Y %H:%M:%S')
+            last_updated += " +0100"
+
+            # Save the README.md file
+            with open(os.path.join("data", f"{repo_name}.md"), 'w') as file:
+                file.write(readme_content)
+            print(f"README.md of the {repo_name} repository downloaded and saved.")
+        else:
+            print(f"Failed to fetch repository info for {repo_name}. Status code: {repo_response.status_code}")
     else:
-        print(f"Failed to fetch README.md for {repo_name}. Status code: {response.status_code}")
+        print(f"Failed to fetch README.md for {repo_name}. Status code: {readme_response.status_code}")
+
+    return last_updated
 
 
 def find_image_references(readme_content):
@@ -129,19 +146,19 @@ def download_images_from_github(image_urls, output_folder):
 
 
 def replace_image_path(md_file, old_path, new_path):
-    with open(md_file, 'r', encoding='utf-8') as file:
+    with open(md_file, 'r') as file:
         content = file.read()
 
     content = re.sub(re.escape(old_path), new_path, content)
 
-    with open(md_file, 'w', encoding='utf-8') as file:
+    with open(md_file, 'w') as file:
         file.write(content)
 
     print(f"old path: {old_path} replaced by new path: {new_path}")
 
 
 def remove_license_section(md_file):
-    with open(md_file, 'r', encoding='utf-8') as file:
+    with open(md_file, 'r') as file:
         content = file.read()
 
     # finding the license section
@@ -153,7 +170,7 @@ def remove_license_section(md_file):
         content = content[:start_index] + content[end_index:]
 
         # saving the updated md file without license
-        with open(md_file, 'w', encoding='utf-8') as file:
+        with open(md_file, 'w') as file:
             file.write(content)
 
         print("License removed successfully.")
@@ -171,7 +188,7 @@ def generate_head(file_name, open_ai_api_key):
 
     new_file_name = file_name.replace("md", " ")
 
-    content = open(os.path.join("data", f"{file_name}.md"), "r", encoding="utf-8").read()
+    content = open(os.path.join("data", f"{file_name}.md"), "r").read()
     try:
         chat_completion = client.chat.completions.create(
             messages=[
@@ -193,7 +210,7 @@ def generate_head(file_name, open_ai_api_key):
                                "e tags che secondo te aiutano il lettore a capire"
                                " di cosa si parla in maniera veloce\n\n"
                                "Testo:"
-                               "\n\n"
+                               "\n"
                                f"{content}"
 
                 }
@@ -201,7 +218,7 @@ def generate_head(file_name, open_ai_api_key):
             model="gpt-3.5-turbo-16k",
         )
         json_text = json.loads(chat_completion.choices[0].message.content)
-        with open(os.path.join("data", "heading", f"{new_file_name}.json"), "w", encoding="utf-8") as file:
+        with open(os.path.join("data", "heading", f"{new_file_name}.json"), "w") as file:
             json.dump(json_text, file, indent=4)
             print(f"File {new_file_name}.json created successfully.")
     except Exception as e:
@@ -211,25 +228,80 @@ def generate_head(file_name, open_ai_api_key):
     time.sleep(22)
 
 
-def markdown_heading_builder(json_data, file_name):
-    with open(json_data, "r", encoding="utf-8") as file:
+def markdown_heading_builder(json_data, file_name, date):
+    with open(json_data, "r") as file:
+
         json_head = json.load(file)
+    try:
+        md_head = f"---" \
+                  f"\ntitle: {str(json_head['title']).replace(':', ';')}" \
+                  f"\ndate: {date}" \
+                  f"\ncategory: [{json_head['category']}]" \
+                  f"\nsubcategories: [{', '.join(json_head['subcategory'])}]" \
+                  f"\ntags: [{', '.join(tag.lower() for tag in json_head['tags'])}]" \
+                  "\n---\n\n"
+        # TODO add the remaining categories
+    except Exception as e:
+        print(e)
+        md_head = f"---" \
+                  f"\ntitle: {str(json_head['title']).replace(':', ';')}" \
+                  f"\ndate: {date}" \
+                  f"\ncategory: [{json_head['category']}]" \
+                  f"\nsubcategories: [{', '.join(json_head['subcategories'])}]" \
+                  f"\ntags: [{', '.join(str(tag.lower()).replace('-', ' ') for tag in json_head['tags'])}]" \
+                  "\n---\n\n"
 
-    md_head = f"---" \
-              f"\ntitle: {json_head['title']}" \
-              f"\ncategory: [{json_head['category']},{json_head['']}]" \
-              f"\nsubcategories: {json_head['subcategories']}" \
-              f"\ntags: {json_head['tags']}" \
-              "\n---\n"
-    # TODO add the remaining categories
-
-    content = open(os.path.join("data", f"{file_name}.md"), "r", encoding="utf-8").read()
+    content = open(os.path.join("data", f"{file_name}.md"), "r").read()
 
     # to be tested
-    new_content = string.Formatter.format(md_head, content)
+    new_content = md_head + content
+    new_content = str(new_content).replace('–', '-')
 
-    with open(os.path.join("data", f"{file_name}.md"), "w") as file:
+    tmp_date = datetime.strptime(date, '%d-%m-%Y %H:%M:%S %z')
+    date_file_naming = tmp_date.strftime('%Y-%m-%d')
+
+    with open(os.path.join("data", f"{date_file_naming}-{file_name}.md"), "w") as file:
         file.write(new_content)
+
+
+def gitUploader(dati_utente, md_file):
+    # Ottieni un'istanza di GitHub usando il token
+    g = Github(dati_utente.get("_Token"))
+
+    sha = None
+
+    repo_name = dati_utente.get("_AccountName") + ".github.io"
+    repo = g.get_user().get_repo(repo_name)
+
+    try:
+        # Prova a ottenere il contenuto del file <mdfile>.md
+        file = repo.get_contents(f"_posts/{md_file}.md")
+        sha = file.sha
+        print(f"Il file {md_file} esiste su GitHub.")
+    except Exception as e:
+        # Se il file non esiste, crealo
+        if "404" in str(e):
+            with open(os.path.join("data", f"{md_file}.md"), "r") as file_content:
+                file_content_str = file_content.read()
+                repo.create_file(f"_posts/{md_file}.md", "Initial file", file_content_str)
+            print(f"File {md_file} creato su GitHub.")
+
+            with open(os.path.join("data", f"{md_file}.md"), "r") as file_content:
+                file = repo.get_contents(f"_posts/{md_file}.md")
+                sha = file.sha
+                file_content_str = file_content.read()
+                repo.update_file(f"_posts/{md_file}.md", "automatic update", file_content_str, sha)
+                print(f"File {md_file} aggiornato su GitHub.")
+                return
+
+    # Se il file esiste, aggiorna il contenuto
+    with open(os.path.join("data", f"{md_file}.md"), "r") as file_content:
+        file = repo.get_contents(f"_posts/{md_file}.md")
+        sha = file.sha
+        file_content_str = file_content.read()
+        repo.update_file(f"_posts/{md_file}.md", "automatic update", file_content_str, sha)
+        print(f"File {md_file} aggiornato su GitHub.")
+
 
 
 def startup():
@@ -273,8 +345,8 @@ def startup():
         json.dump(lista_repo, lista_repo_file)
 
     for repo in lista_repo:
-        download_readme("softlab-unimore", repo, user_data.get("_Token"))
-        paths = find_image_references(open(os.path.join("data", f"{repo}.md"), "r", encoding="utf-8").read())
+        project_date = download_readme("softlab-unimore", repo, user_data.get("_Token"))
+        paths = find_image_references(open(os.path.join("data", f"{repo}.md"), "r").read())
         for image in paths:
             if not (download_images_from_github(
                     [f"https://raw.githubusercontent.com/softlab-unimore/{repo}/main/{image}" for image in paths],
@@ -285,8 +357,13 @@ def startup():
 
             replace_image_path(os.path.join("data", f"{repo}.md"), image, f"images/{os.path.basename(image)}")
         remove_license_section(os.path.join("data", f"{repo}.md"))
-        generate_head(repo, user_data.get("_OpenAIKey"))
-        # markdown_heading_builder(os.path.join("data", "heading", f"{repo}.json"))
+        # generate_head(repo, user_data.get("_OpenAIKey"))
+        markdown_heading_builder(os.path.join("data", "heading", f"{repo}.json"), repo, project_date)
+
+        tmp_date = datetime.strptime(project_date, '%d-%m-%Y %H:%M:%S %z')
+        date_path = tmp_date.strftime('%Y-%m-%d')
+
+        # gitUploader(user_data, f"{date_path}-{repo}")
 
 
 if __name__ == "__main__":
